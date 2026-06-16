ddd3779517981fb2f25dbb141cfd562ad819518e
Cefise <vynii_80@unochapeco.edu.br>
2026-06-15 21:02:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@
 // Inicialização do Supabase
 const SUPABASE_URL = 'https://oooedutvxendobfqiinlj.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9vZWR1dHZ4ZW5kb2JmcWlpbmxqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE1NDg3NjAsImV4cCI6MjA5NzEyNDc2MH0.KSuRBgH3mLDP0zcdl4v6TVvG3Kt_5y6aXOQFDxC8O8E';
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+var supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
 let currentUser = null;
 let currentProfile = null;
